684df489109ca69c0fa871c5b42c8a68fc4447d7
2theMoon <dlaguddh1@gmail.com>
2022-02-22 16:48:58 +0900

    subscription: 데이터가 수정될때 마다 감지해서 인자로 주어지는 함수를 실행

--- vanila-redux/src/index.js
+++ vanila-redux/src/index.js
@@ -4,11 +4,28 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  console.log(state)
-  return state;
-};
+number.innerText = 0;
 
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    if (count <= 0) {
+      return count;
+    } else {
+      return count - 1;
+    }
+  } else {
+    return count;
+  }
+};
 const countStore = createStore(countModifier);
 
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
 
