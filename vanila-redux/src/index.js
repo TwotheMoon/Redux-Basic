59a34787f4ebcc2d70d440824a69b852098e727c
2theMoon <dlaguddh1@gmail.com>
2022-02-22 16:26:59 +0900

    store: 데이터 저장소, 값의 수정은 함수를 만들어 그 안에서만 수정

---
+++ vanila-redux/src/index.js
@@ -0,0 +1,14 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (state = 0) => {
+  console.log(state)
+  return state;
+};
+
+const countStore = createStore(countModifier);
+
+
